29ab45b7efa5355b9fb1030b48c16456baa6a262
Ashley <Ashley@192.168.1.91>
2017-11-20 22:36:36 +0000

    updated triangle program and added TwoDShapes outline

--- src/Shape/Triangle.java
+++ src/Shape/Triangle.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Shape;
+package shape;
 
-/**
- *
- * @author Ashley
- */
-public class Triangle {
-    
+public class Triangle extends TwoDShapes {
+
+    /**
+     * @auther Ashley
+     */
+
+    private final double _height;
+    private final double _width;
+
+    public Triangle(String name, double height, double width) {
+        super(4, name);
+        _height = height;
+        _width = width;
+    }
+
+    @Override
+    public double getArea() {
+        // TODO Auto-generated method stub
+        return _height * _width / 2;
+    }
+
+    @Override
+    public double getPerimiter() {
+        // TODO Auto-generated method stub
+        return _height + _width + Math.sqrt(Math.pow(_height, 2) + Math.pow(_width, 2));
+    }
+
 }
