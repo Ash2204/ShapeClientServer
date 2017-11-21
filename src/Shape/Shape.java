5456e4e2890515c5806380ad1571321e21f014ee
Ashley <Ashley@192.168.1.91>
2017-11-21 13:29:31 +0000

    updated Shape

--- src/Shape/Shape.java
+++ src/Shape/Shape.java
@@ -8,37 +8,38 @@ import java.util.LinkedList;
  */
 public abstract class Shape {
 
-  final int _numberOfSides;
-  final String _name;
-  
-  public Shape(int numberOfSides, String name){
-      _numberOfSides = numberOfSides;
-      _name = name;
-  }
-  
-  public abstract double getArea();
-  
-  public abstract double getPerimiter();
-  
-  public void displayDescrpition(){
-      System.out.println("I'm a shape name " + _name);
-  }
+    final int _numberOfSides;
+    final String _name;
+
+    public Shape(int numberOfSides, String name) {
+        _numberOfSides = numberOfSides;
+        _name = name;
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimiter();
+
+    public void displayDescrpition() {
+        System.out.println("I'm a shape name " + _name);
+    }
+
     public static void main(String[] args) {
-        
+
         final LinkedList<Shape> shapes = new LinkedList();
-        
+
         shapes.add(new Rectangle("Rectangle one", 10, 8));
         shapes.add(new Rectangle("Rectangle two", 20, 5));
         shapes.add(new Circle("Circle one", 10));
         shapes.add(new Circle("Circle two", 20));
-        
+
         for (final Shape shape : shapes) {
             shape.displayDescrpition();
-            
+
             if (shape instanceof Circle) {
                 System.out.println("Area: " + shape.getArea());
                 System.out.println("Circumference:" + shape.getPerimiter());
-            }   else {
+            } else {
                 System.out.println("Area: " + shape.getArea());
                 System.out.println("Perimiter: " + shape.getPerimiter());
             }
@@ -46,4 +47,3 @@ public abstract class Shape {
         }
     }
 }
-
