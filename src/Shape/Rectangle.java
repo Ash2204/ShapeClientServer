d9cd73cd79585a2f7cc71bf2f0c2b3230bd2cb22
Ashley <Ashley@192.168.1.91>
2017-11-21 15:10:20 +0000

    Updated Rectangle serial UID

--- src/Shape/Rectangle.java
+++ src/Shape/Rectangle.java
@@ -4,11 +4,12 @@ package shape;
  *
  * @author Ashley
  */
-public class Rectangle extends Shape {
+public class Rectangle extends TwoDShapes {
     
-    private final double _height;
-    private final double _width;
-    
+                 private static final long serialVersionUID = -5138161872652033101L;
+                 private final double _height;
+                 private final double _width;
+
     public Rectangle(String name, double height, double width){
         super(4, name);
         _height = height;
@@ -21,11 +22,26 @@ public class Rectangle extends Shape {
         System.out.println("\tI'm also a rectangle");
     }
    
-    public double getArea(){
+   /* public double getArea(){
         return _height * _width;
     }
     
     public double getPerimiter(){
         return (_height + _width) * 2;
-    }
+    }*/
+
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return _height * _width;
+	}
+
+
+	@Override
+	public double getPerimiter() {
+		// TODO Auto-generated method stub
+		return (_height + _width) * 2;
+	}
 }
+
