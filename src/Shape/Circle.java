76346395685eace4970162596a1366f79db9b7eb
Ashley <Ashley@192.168.1.91>
2017-11-21 15:07:32 +0000

    Updated Circle serial UID

--- src/Shape/Circle.java
+++ src/Shape/Circle.java
@@ -1,13 +1,13 @@
-
 package shape;
 
 /**
  *
  * @author Ashley
  */
-public class Circle extends Shape{
+public class Circle extends TwoDShapes{
     
-    private final double _radius;
+	private static final long serialVersionUID = -7024656029027547426L;
+	private final double _radius;
     
     public Circle(String name, double radius) {
         super(1, name);
@@ -19,11 +19,30 @@ public class Circle extends Shape{
         System.out.println("I'm also a circle");
     }
     
-    public double getArea() {
+  /*  public double getArea() {
         return Math.PI * Math.pow(_radius, 2);
     }
     
     public double getPerimiter(){
         return Math.PI * (_radius * 2);
-    }
+    }*/
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return Math.PI * Math.pow(_radius, 2);
+	}
+
+	@Override
+	public double getPerimiter() {
+		// TODO Auto-generated method stub
+		return Math.PI * (_radius * 2);
+	}
+	
+	@Override
+	public String toString() 
+	{
+		return "C," + this._name + "," + this._numberOfSides + "," + this._radius; 
+	}
 }
+
