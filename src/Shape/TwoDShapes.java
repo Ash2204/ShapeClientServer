cc66e9261be7ce331f76a6edc9784cc4dd52906c
Ashley <Ashley@192.168.1.91>
2017-11-21 15:20:17 +0000

    updated serial UID to Two D shapes

--- src/Shape/TwoDShapes.java
+++ src/Shape/TwoDShapes.java
@@ -1,12 +1,14 @@
 package shape;
 
+/**
+ *
+ * @author Ashley
+ */
+
 public abstract  class TwoDShapes  extends Shape  {
-
-	/**
-	 * 
-	 */
 	
-
+                 private static final long serialVersionUID = 2606303875808957516L;
+                 
 	public TwoDShapes(int numberOfSides, String name) {
 		super(numberOfSides, name);
 		// TODO Auto-generated constructor stub
