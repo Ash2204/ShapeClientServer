e203c468a4c0ba5a55100c147ccf506cc64bfbf7
Ashley <Ashley@192.168.1.91>
2017-11-21 13:28:09 +0000

    Added ThreeDShapes

---
+++ src/Shape/ThreeDShapes.java
@@ -0,0 +1,36 @@
+package shape;
+
+public abstract class ThreeSections extends Shape  {
+
+	/**
+	 * @auther Ashley
+	 */
+	private static final long serialVersionUID = -3644095966436278837L;
+
+	public ThreeSections(int numberOfSides, String name) {
+		super(numberOfSides, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	 public void displayDescription(){
+	        super.displayDescrpition();
+	        System.out.println("\tI'm also a TwoDShapes");
+	    }
+	 
+	 public abstract double getSurface ();
+	  
+	 public abstract double getvolume();
+	 
+	/*@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getPerimiter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+*/
+}
