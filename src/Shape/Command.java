259184acae95609ad6d633dcf449f0c8a0355815
Ashley <Ashley@192.168.1.91>
2017-11-21 15:12:08 +0000

    Updated command serial UID

--- src/Shape/Command.java
+++ src/Shape/Command.java
@@ -9,7 +9,8 @@ import java.io.Serializable;
 
 public class Command extends Object implements Serializable  {
 
-	public String cmd_type;
+	private static final long serialVersionUID = -8676948476358035517L;
+                  public String cmd_type;
 	public Object data;
 }
 
