01c8e2155a6659137c0641d560e1e1e531a26b76
Ashley <Ashley@192.168.1.91>
2017-11-21 14:24:23 +0000

    Created a simple command method

---
+++ src/Shape/Command.java
@@ -0,0 +1,15 @@
+package shape;
+
+/**
+ *@author Ashley 
+ */
+
+
+import java.io.Serializable;
+
+public class Command extends Object implements Serializable  {
+
+	public String cmd_type;
+	public Object data;
+}
+
